0a06e1af2f9ca4df786644e4f6e13292523336d9
supercreammm <a0905786053@gmail.com>
2025-12-21 04:55:20 +0800

    Merge my files to main

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,7 +1,15 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// ★ 1. 引入簽名套件
+import VueSignaturePad from 'vue-signature-pad'
+
+const app = createApp(App)
+
+app.use(router)
+// ★ 2. 註冊簽名套件
+app.use(VueSignaturePad)
+
+app.mount('#app')
